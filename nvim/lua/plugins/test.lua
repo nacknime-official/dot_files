65d51f312fec331af12cf536d9ca435b550e9f89
nacknime-official <nacknime.official@gmail.com>
2023-04-15 01:37:56 +0300

    fix(nvim): forgot to surround key for `TestNearest`

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -1,6 +1,6 @@
 return {
   "vim-test/vim-test",
-  keys = { "<leader>tn", "<cmd>TestNearest<CR>" },
+  keys = { { "<leader>tn", "<cmd>TestNearest<CR>" } },
   config = function()
     vim.g["test#strategy"] = 'neovim'
     vim.g["test#go#gotest#options"] = '-v'
