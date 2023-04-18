442af6f79adf8b93182fe0c854ea30282f32ca0d
nacknime-official <nacknime.official@gmail.com>
2023-04-19 00:13:01 +0300

    nvim: add `select_refactor` mapping

--- nvim/lua/plugins/refactoring.lua
+++ nvim/lua/plugins/refactoring.lua
@@ -1,11 +1,20 @@
 return {
     "theprimeagen/refactoring.nvim",
-    keys = { { "<leader>ri", mode = { "v" } } },
+    keys = {
+        { "<leader>rr", mode = "v" },
+        { "<leader>ri", mode = "v" },
+    },
     config = function()
         require('refactoring').setup({})
 
         vim.api.nvim_set_keymap("v", "<leader>ri",
             [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]],
             { noremap = true, silent = true, expr = false })
+        vim.api.nvim_set_keymap(
+            "v",
+            "<leader>rr",
+            ":lua require('refactoring').select_refactor()<CR>",
+            { noremap = true, silent = true, expr = false }
+        )
     end
 }
